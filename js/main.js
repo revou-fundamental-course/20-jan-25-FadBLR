aecc0a2319a09f13c1726372cb6ce335d6755366
Fazuu_ <dafaablitar@gmail.com>
2025-01-29 11:40:56 +0700

    final update + comment

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
+// fungsi untuk menghitung BMI
 function hitung_BMI (event) {
 
+    // menghentikan submit default dari form (yang biasanya akan mereset halaman)
     event.preventDefault();
 
+    // mengambil nilai dari input berat badan dan tinggi badan
     let berat_badan = parseFloat(document.getElementById('weight').value);
     let tinggi_badan = parseFloat(document.getElementById('height').value);
 
+    // menghitung BMI
     let bmi = berat_badan / (tinggi_badan * tinggi_badan);
 
+    // menentukan kategori BMI berdasarkan nilai BMI
     let category = ''; 
-
     if (bmi < 18) {
         category = 'Kurus';
     } else if (bmi >= 18 && bmi < 25) {
@@ -19,7 +23,7 @@ function hitung_BMI (event) {
         category = 'Obesitas';
     }
 
+    // menampilkan nilai BMI dan kategori BMI
     document.getElementById('bmi').textContent = bmi.toFixed(2);
     document.getElementById('category').textContent = category;
-
 }
